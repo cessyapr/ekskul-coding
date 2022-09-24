b6f0a77962643ed5136e5286c50e55484c2c297c
cessyapr <113175971+cessyapr@users.noreply.github.com>
2022-09-24 07:00:29 +0700

    Create injun.cpp

---
+++ injun.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	cout<< " Dear                                                                                                                              \n";
+	cout<< " Renjun                                                                                                                           \n";
+	cout<< " Di rumah                                                                                                                       \n\n";
+	
+	
+	
+	cout<< " cuma mau bilang                                                                                                                   \n";
+	cout<< " kamu ganteng aku suka                                                                                                             \n";
+	cout<< " udahh itu aja                                                                                                                     \n";
+	cout<< " sekian trima gaji                                                                                                                 \n";
+	cout<< " byee                                                                                                                            \n\n";
+	
+	
+	cout<< "\t\t\t\t Palembang 16 September 2022 \n";
+	cout<< "\t\t\t\t ytta   \n";
+	
+	getch();
+	return 0;
+	
+}
